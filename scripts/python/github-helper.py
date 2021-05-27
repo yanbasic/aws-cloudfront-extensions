7f6d4a8dff40fdba0c450b6f23777a93d71f688d
Mike Yan <yanbasic@gmail.com>
2021-05-27 14:02:10 +0800

    Update github-helper.py

--- scripts/python/github-helper.py
+++ scripts/python/github-helper.py
@@ -7,8 +7,8 @@ from github import Github
 # First create a Github instance:
 
 # using an access token
-g = Github(sys.argv[0])
-print(sys.argv[0])
+g = Github(sys.argv[1])
+print(sys.argv[1])
 
 SECRETS_MANAGER = boto3.client('secretsmanager')
 
