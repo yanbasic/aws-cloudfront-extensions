45d09e3de930d7ad3508c488a4f516a2a6ccb15b
Mike Yan <yanbasic@gmail.com>
2021-05-28 16:02:08 +0800

    Update github-helper.py

--- scripts/python/github-helper.py
+++ scripts/python/github-helper.py
@@ -2,6 +2,7 @@ import re
 
 import boto3
 import sys
+import os
 from github import Github
 
 # First create a Github instance:
@@ -15,8 +16,13 @@ SECRETS_MANAGER = boto3.client('secretsmanager')
 # Github Enterprise with custom hostname
 # g = Github(base_url="https://github.com/api/v3", login_or_token="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")
 
+CODEBUILD = boto3.client('codebuild')
+print('=====================')
+print(os.getenv('CODEBUILD_BUILD_ID'))
+CODEBUILD.batch_get_builds(ids=[os.getenv('CODEBUILD_BUILD_ID')])
 
-print(g.get_user())
+
+python-build-cloudfront-extensions:b8e6834d-0204-417b-8ab9-413b6ccfd009
 # Then play with your Github objects:
 for repo in g.get_user().get_repos():
     if repo.name == 'aws-cloudfront-extensions':
