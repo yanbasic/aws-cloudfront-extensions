2b6ec1f35dee2ad1e59f8bef1dee2df437774be7
Mike Yan <yanbasic@gmail.com>
2021-05-28 16:14:08 +0800

    Update github-helper.py

--- scripts/python/github-helper.py
+++ scripts/python/github-helper.py
@@ -20,6 +20,7 @@ CODEBUILD = boto3.client('codebuild')
 print('=====================')
 print(os.getenv('CODEBUILD_BUILD_ID'))
 response = CODEBUILD.batch_get_builds(ids=[os.getenv('CODEBUILD_BUILD_ID')])
+print(response)
 build_details = response['builds'][0]
 print(build_details)
 matches = re.match(r'^pr\/(\d+)', build_details.get('sourceVersion', ""))
