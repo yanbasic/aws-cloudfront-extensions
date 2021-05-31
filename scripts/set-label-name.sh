08548ff87f306b638046c2e6299ba611b347ac61
Mike Yan <yanbasic@gmail.com>
2021-05-31 13:08:26 +0800

    Update set-label-name.sh

--- scripts/set-label-name.sh
+++ scripts/set-label-name.sh
@@ -2,6 +2,8 @@
 
 pip install PyGithub
 export labelName=`python scripts/python/set-label-name.py $GHToken`
+echo `python scripts/python/set-label-name.py $GHToken`
+echo aaaaaaaaaaaaaa
 python scripts/python/set-label-name.py $GHToken
 labelName=$(python scripts/python/set-label-name.py $GHToken $GHToken)
 export labelName=$labelName
