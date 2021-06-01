065af88a256977f0ec0dc3caeaa5539ad3b934bb
Mike Yan <yanbasic@gmail.com>
2021-06-01 10:11:04 +0800

    Update app.js

--- edge/nodejs/access-origin-by-weight-rate/access-origin-by-weight-rate/app.js
+++ edge/nodejs/access-origin-by-weight-rate/access-origin-by-weight-rate/app.js
@@ -9,7 +9,7 @@ var weights = originList.map(function (originList) {
 
 /* This is an origin request function */
 exports.handler = (event, context, callback) => {
-    console.log('Handler start!');
+    console.log('Handler start!111');
     const request = event.Records[0].cf.request;
 
     //select origin according to weight rate
